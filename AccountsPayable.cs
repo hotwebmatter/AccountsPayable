db4c357fd5326976d01211552d42d1b7b4da10ba
Matt Obert on Windows <matt@hotwebmatter.com>
2019-11-24 16:52:13 -0500

    Override ToString() method for AccountsPayable class

--- AccountsPayable.cs
+++ AccountsPayable.cs
@@ -23,5 +23,23 @@ namespace AccountsPayable
             set { namesArray = value; }
         }
 
+        public override string ToString()
+        {
+            double totalPayables = 0;
+            double highestPayable = 0;
+            string result = String.Format("Company Payables\n");
+            for (int i = 0; i < namesArray.Length; i++)
+            {
+                result += String.Format("{0}\t{1:c}\n", namesArray[i], payableArray[i]);
+                totalPayables += payableArray[i];
+                if (payableArray[i] > highestPayable)
+                {
+                    highestPayable = payableArray[i];
+                }
+            }
+            result += String.Format("Total payables: {0:c}\n", totalPayables);
+            result += String.Format("Highest payable: {0:c}\n", highestPayable);
+            return result;
+        }
     }
 }
