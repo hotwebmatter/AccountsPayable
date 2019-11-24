3eccc6b2c758a8c66be995ff8beb3eb33114a6cf
Matt Obert on Windows <matt@hotwebmatter.com>
2019-11-24 16:22:56 -0500

    Stub out AccountsPayable class with setters

---
+++ AccountsPayable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountsPayable
+{
+    class AccountsPayable
+    {
+        private string[] namesArray;
+        private double[] payableArray;
+
+        public string[] NamesArray
+        {
+            set { namesArray = value; }
+        }
+        public string[] PayableArray
+        {
+            set { payableArray = value; }
+        }
+    }
+}
